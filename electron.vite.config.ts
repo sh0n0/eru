76fdeb923666d960c335e1c4fd69c9dad92ae965
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2026-05-10 19:07:49 +0900

    Add biome

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,16 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "electron-vite";
 
 export default defineConfig({
-  main: {},
-  preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
-    },
-    plugins: [react()]
-  }
-})
+	main: {},
+	preload: {},
+	renderer: {
+		resolve: {
+			alias: {
+				"@renderer": resolve("src/renderer/src"),
+			},
+		},
+		plugins: [react()],
+	},
+});
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,16 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+const root = document.getElementById("root");
+if (!root) {
+	throw new Error("Failed to find the root element");
+}
+
+createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
